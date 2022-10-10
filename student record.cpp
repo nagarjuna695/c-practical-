289021825b6a30ab90f239d7c991c22ee5b1337d
nagarjuna695 <112924809+nagarjuna695@users.noreply.github.com>
2022-10-10 06:44:40 +0530

    Create student record.cpp

---
+++ student record.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()student record.cpp
+{
+	int tot,stf,std,def;
+	printf("enter the total number of users: ",tot);
+	scanf("%d",&tot);
+	
+	printf("enter total staff user : ",stf);
+	scanf("%d",&stf);
+	def=stf/3;
+	std=tot-stf-def;
+	printf("the sudent users are: %d",std);
+}student record.cpp
